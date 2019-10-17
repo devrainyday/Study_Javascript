2619337795cea1c4c6d872deab79b353545bd775
김예원 <kyw017763@gmail.com>
2019-10-17 15:19:25 +0900

    Study little bit

---
+++ consolePrac.js
@@ -0,0 +1,54 @@
+let foo = 'woni';
+let bar = 0;
+
+// local에서 실행시 {} (빈 객체),
+// Chrome에서 실행시 Window 객체 출력
+console.log(this); 
+console.dir(this);
+
+console.log({name: 'woni', age: 28}); 
+console.dir({name: 'woni', age: 38});
+
+console.info(`${foo}, ${bar}`);
+
+console.warn(`${foo}, ${bar}`); // 노란색 경고 아이콘
+console.error(`${foo}, ${bar}`); // 빨간색 아이콘
+
+console.time();
+setTimeout(() => { console.timeEnd(); }, 2000);
+
+console.time("woni");
+console.timeEnd("woni");
+
+// css 적용 ( %c 는 css 의 약자, 두번째 매게변수에 css 정의
+// %d : 숫자
+// %o : 객체
+// %s : 문자
+console.log('%c %s', 'color: green', `${foo}`);
+console.log('%c %s', 'color: blue', `${foo}`); 
+console.log('%c %o', 'color: yellow', `aaa ${foo}`); 
+console.log('%c %o', 'color: yellow', {name: 'woni', age: 28}); // Object {name: "woni", age: 28}
+console.log('%c %s', 'color: yellow', {name: 'woni', age: 38}); // Object
+
+console.assert(true == false, 'false');
+console.assert(true == false, ['false', 'second element']);
+
+console.group('firstGroup');
+console.log('a');
+console.log('b');
+console.log('c');
+console.log('d');
+console.log('e');
+console.groupEnd('firstGroup');
+
+console.group('Group A');
+console.log('첫번째 메세지');
+console.group('Group B');
+console.log('하위 메세지 a');
+console.log('하위 메세지 b');
+console.groupEnd();
+console.log('두번째 메세지');
+console.groupEnd();
+
+// Console was cleared
+// setTimeout(() => {console.clear()}, 1000);
--- ex01_frequency_array.js
+++ ex01_frequency_array.js
@@ -11,25 +11,22 @@ const countFrequency = str => {
         return '함수에 전달된 값이 문자열이 아닙니다!';
     }
 
+    resultName.push(str[0]);
+    resultNum.push(1);
+
     // 문자열 순회
     for(let i = 0; i < str.length; i++) {
         value = str[i];
         
-        // result 배열들에 아무 값이 없을 경우 for문이 정상적으로 작동하지 않으므로 따로 분리함
-        if(i === 0) {
-            resultName.push(value);
-            resultNum.push(1);
-        }
-    
         for(let j = 0; j < resultName.length; j++) {
             resultValue = resultName[j];
-    
+
             if(value === resultValue) {
                 resultNum[j]++;
                 break;
             }
     
-            if(j === resultName.length-1) {
+            if(j === resultName.length) {
                 resultName.push(value);
                 resultNum.push(1);
             }
