883eb6bbd04632680b420a0793ec16a61dc0241c
Yewon Kim <37951612+kyw017763@users.noreply.github.com>
2019-10-30 02:42:44 +0900

    Practice object method

---
+++ objectPrac.js
@@ -0,0 +1,26 @@
+const fruitList = [
+    {name: 'grape', level: 3},
+    {name: 'lemon', level: 1},
+    {name: 'apple', level: 5},
+    {name: 'melon', level: 7},
+    {name: 'watermelon', level: 6},
+    {name: 'orange', level: 3}
+];
+
+console.log(fruitList[0].name);
+
+const objectKeys = Object.keys(fruitList[0]);
+fruitList.forEach(oArrayE => {
+    objectKeys.forEach(e => {
+        console.log(oArrayE[e]);
+    });
+    console.log();
+});
+
+const a = 'a';
+let cnt = 0;
+
+let object = {
+    [a + ++cnt]: ++cnt
+};
+console.log(object);
---
+++ objectReflectApiPrac.js
@@ -0,0 +1,62 @@
+// Reflect 객체 안에 있는 메소드
+
+const fruitObj = {name: 'grape', level: 3};
+const fruitArr = [
+    {name: 'grape', level: 3},
+    {name: 'lemon', level: 1},
+    {name: 'apple', level: 5},
+    {name: 'melon', level: 7},
+    {name: 'watermelon', level: 6},
+    {name: 'orange', level: 3}
+];
+
+console.log(Reflect.get(fruitObj, 'name'));
+console.log(Reflect.get(fruitObj, 'level'));
+
+console.log(Reflect.get(fruitArr, 0));
+
+Reflect.set(fruitObj, 'level', {'min': 2, 'max': 6});
+console.log(JSON.stringify(fruitObj));
+
+const multipleSelf = (a) => a*a;
+console.log(Reflect.apply(multipleSelf, null, [5]));
+// 두 번째 인자는 함수의 this 를 바꾸고 싶을 때 사용함
+
+function Person(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+}
+const woni = Reflect.construct(Person, ['woni', '!']);
+
+console.log(Reflect.has(fruitObj, 'value')); // true
+
+console.log(Reflect.deleteProperty(fruitObj, 'level')); // true
+console.log(fruitObj);
+
+// 속성 설정
+Reflect.defineProperty(fruitObj, 'name', {
+    // configurable
+    // enumerable
+    // writable
+    enumerable: false
+});
+
+// 속성 설정 가져옴
+console.log(Reflect.getOwnPropertyDescriptor(fruitObj, 'name'));
+
+const author = {
+    bookTitle: ['Guns, Germs, and Steel'],
+    write() { console.log('Write the book'); },
+    read() { console.log('Read the book'); }
+};
+
+Reflect.setPrototypeOf(woni, author); 
+// woni의 prototype이 author 가 됨
+
+console.log(Reflect.getPrototypeOf(woni));
+// {bookTitle: Array(1), write: , read: }
+
+Reflect.preventExtensions(fruitObj); // true
+console.log(Reflect.isExtensible(fruitObj)); // false
+
+console.log(Reflect.ownKeys(fruitObj));
